742761362e2678547f053f5be32a9ea99065f57a
siegan-g <siegangovender@outlook.com>
2026-07-26 01:37:06 +0200

    init emacs config

---
+++ emacs/init.el
@@ -0,0 +1,45 @@
+;; ############################
+;; Package Management 
+;; ############################
+
+;; bootstrap straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+;; set use-package to invoke from straight.el
+(setq straight-use-package-by-default t)
+(straight-use-package  'use-package)
+
+
+(load (expand-file-name "packages.el" user-emacs-directory))
+
+;; ############################
+;; Configuration 
+;; ############################
+
+(setq ring-bell-function 'ignore)
+
+(global-display-line-numbers-mode 1)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+(set-face-attribute 'default nil :height 150)
+
+(which-key-mode 1)
+
+(setq dired-dwim-target t)
---
+++ emacs/packages.el
@@ -0,0 +1,136 @@
+;; ##################
+;; packages
+;; ##################
+;; vim keybindings
+(use-package evil
+  :straight t
+  :init
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1)
+  (evil-set-undo-system 'undo-redo))
+
+(use-package evil-collection
+  :straight t
+  :after evil
+  :init
+  (evil-collection-init))
+
+(use-package evil-commentary
+  :straight t
+  :config
+  (evil-commentary-mode))
+
+(use-package evil-mc
+  :straight t
+  :config
+  (evil-mc-mode 1))
+
+;; git
+(use-package magit
+  :straight t)
+
+(use-package diff-hl
+  :straight t
+  :init
+  (global-diff-hl-mode))
+
+(use-package blamer
+  :straight t
+  :bind (("C-c i" . blamer-show-commit-info))
+  :defer 20
+  :custom
+  (blame-idle-time 0.1)
+  (blame-min-offset 70)
+  :config
+  (global-blamer-mode 1))
+
+;; autocompletion 
+(use-package ivy
+  :straight t
+  :demand t
+  :config
+  (ivy-mode 1))
+
+;; project exploreer
+(use-package treemacs
+  :straight t
+  :defer t
+  :init
+  (with-eval-after-load `winum
+    (define-key winum-keymap(kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-follow-after-init t
+	  treemacs-width 35
+	  treemacs-show-hidden-files t)
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+  ))
+
+(use-package treemacs-evil
+  :after (treemacs evil)
+  :straight t)
+
+(use-package treemacs-magit
+  :after (treemacs magit)
+  :straight t)
+
+;; window management
+(use-package winum
+  :straight t
+  :config
+  (winum-mode 1))
+
+;;terminal
+(use-package vterm
+  :straight t)
+
+;; lsp
+;; (straight-use-package 'project)
+;; (straight-use-package 'flymake)
+
+(use-package eglot
+  :straight (:type built-in)
+  :hook ((python-mode . eglot-ensure)
+	 (rust-mode . eglot-ensure)
+	 (markdown-mode . eglot-ensure)))
+
+(use-package corfu
+:straight t
+:init
+(global-corfu-mode))
+
+;; themes
+(use-package kaolin-themes
+  :config
+  (load-theme `kaolin-dark t)
+  (kaolin-treemacs-theme))
+
+(use-package all-the-icons
+  :straight t
+  :if (display-graphic-p))
+
+(use-package doom-modeline
+  :straight t
+  :init
+  (doom-modeline-mode 1))
+
+
+;; TODO
+;; Workspaces
+;; (use-package persp-mode
+;;   :straight t)
+;; treemacs-persp
+
+;; project management
+;; (use-package projectile
+;;   :straight t)
+;; treemacs-projectile
+
+
+;; other
+(use-package dashboard
+  :straight t
+  :config
+  (dashboard-setup-startup-hook))
