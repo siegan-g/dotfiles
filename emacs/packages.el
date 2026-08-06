3566cbdb75a3d6726e19d7877ccca6a313e52797
siegan-g <siegangovender@outlook.com>
2026-08-06 21:34:54 +0200

    fix issues with diff-hl

--- emacs/packages.el
+++ emacs/packages.el
@@ -32,8 +32,11 @@
 
 (use-package diff-hl
   :straight t
+  :demand t
   :init
-  (global-diff-hl-mode))
+  (global-diff-hl-mode)
+  :hook
+  (magit-post-refresh-hook . diff-hl-magit-post-refresh))
 
 (use-package blamer
   :straight t
